beb4abcda2526dbd5931a9498dec222bcbc5c0df
Jacob <simerly81@gmail.com>
2024-02-24 12:03:04 -0500

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ecs_engine',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[],
     
